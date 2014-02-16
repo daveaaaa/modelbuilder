1b3c93f6ba30307848c2769ea8931d1f7af08bd7
David Armstrong <d_armstrong01@hotmail.co.uk>
2014-02-16 15:03:56 +0000

    CSTable now creates a CS table representation of the database table

--- ModelBuilder/src/modelbuilder/tables/CSTable.java
+++ ModelBuilder/src/modelbuilder/tables/CSTable.java
@@ -10,18 +10,20 @@ import modelbuilder.Language;
  *
  * @author david
  */
-public class CSTable extends Table{
-    
-    public CSTable(String name){
-         super(name, "int", "Date", "String", "double", "boolean", Language.CS);
-    }
-    
-    @Override
-    public String buildTable() {
-        return "";
+public class CSTable extends Table {
+
+    public CSTable(String name) {
+        super(name, "int", "Date", "String", "double", "boolean", Language.CS);
     }
 
-    
+    @Override
+    public String buildTable() {
+        String tableModel = buildClassHeader() + "\n";
+        tableModel += createAttributes();
+        tableModel += buildClassFooter();
+        return tableModel;
+    }
+
     @Override
     protected String buildClassHeader() {
         return "public class " + name + " {\n";
@@ -31,10 +33,17 @@ public class CSTable extends Table{
     protected String buildClassFooter() {
         return "}";
     }
-    
+
     @Override
     protected String buildAttribute(String datatype, String identifier) {
-        return "";
+        String smallIdentifier = identifier.substring(0, 1).toLowerCase() + identifier.substring(1, identifier.length());
+
+        String returnString = "\tprivate " + datatype + " _" + smallIdentifier + ";\n";
+        returnString += "\tpublic " + datatype + " " + identifier + " {\n";
+        returnString += "\t" + addCSGetter(datatype, smallIdentifier) + "\n";
+        returnString += "\t" + addCSSetter(datatype, smallIdentifier) + "\n";
+        returnString += "\t}\n";
+        return returnString;
     }
 
     @Override
@@ -44,13 +53,22 @@ public class CSTable extends Table{
 
     @Override
     protected String buildGetter(String datatype, String identifier) {
-        return ""; 
+        return "";
     }
 
     @Override
     protected String toJSON() {
-        return ""; 
+        return "";
     }
 
-    
+    private String addCSSetter(String datatype, String identifier) {
+        String returnString = "\tset { this._" + identifier + " = value; }";
+        return returnString;
+    }
+
+    private String addCSGetter(String datatype, String identifier) {
+        String returnString = "\tget { return this._" + identifier + " ; }";
+        return returnString;
+    }
+
 }
